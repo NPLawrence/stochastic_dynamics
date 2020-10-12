266c11392fce4b020292fef6cf39e4c25512acc0
Nathan Lawrence <nplawrence@dhcp-206-12-165-154.ubcsecure.wireless.ubc.ca>
2020-10-12 12:23:34 -0700

    Added requirements.txt

--- generate_data.py
+++ generate_data.py
@@ -3,7 +3,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import control.matlab
 import torch
 from torch.utils.data import DataLoader, Dataset
 from sklearn.model_selection import train_test_split
